1a3cc92b915b31d718808e3bf75436e07ee29f78
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2024-07-09 12:03:52 -0400

    funciones ReplayMenuWidget

--- Source/PlaystageDemo6/ReplayMenuWidget.cpp
+++ Source/PlaystageDemo6/ReplayMenuWidget.cpp
@@ -2,4 +2,62 @@
 
 
 #include "ReplayMenuWidget.h"
+#include "ReplayPlayerController.h"
+#include "Engine/World.h"
+#include "Misc/FileHelper.h"
+#include "Misc/Paths.h"
 
+void UReplayMenuWidget::StartRecording()
+{
+    if (AReplayPlayerControllerB* PlayerController = Cast<AReplayPlayerControllerB>(GetWorld()->GetFirstPlayerController()))
+    {
+        PlayerController->StartRecording();
+    }
+}
+
+void UReplayMenuWidget::StopRecording()
+{
+    if (AReplayPlayerControllerB* PlayerController = Cast<AReplayPlayerControllerB>(GetWorld()->GetFirstPlayerController()))
+    {
+        PlayerController->StopRecording();
+    }
+}
+
+void UReplayMenuWidget::ShowReplayList()
+{
+    TArray<FString> ReplayList = GetReplayList();
+    // Populate your ScrollBox or ListView with ReplayList
+}
+
+TArray<FString> UReplayMenuWidget::GetReplayList()
+{
+    TArray<FString> ReplayList;
+    IFileManager& FileManager = IFileManager::Get();
+    FString ReplayDirectory = FPaths::ProjectSavedDir() / TEXT("Demos");
+    FileManager.FindFiles(ReplayList, *ReplayDirectory, TEXT("*.demo"));
+    return ReplayList;
+}
+
+void UReplayMenuWidget::SearchReplay(const FString& SearchText)
+{
+    TArray<FString> ReplayList = GetReplayList();
+    ReplayList.RemoveAll([&SearchText](const FString& Replay) {
+        return !Replay.Contains(SearchText);
+        });
+    // Populate your ScrollBox or ListView with filtered ReplayList
+}
+
+void UReplayMenuWidget::RenameReplay(const FString& OldName, const FString& NewName)
+{
+    FString ReplayDirectory = FPaths::ProjectSavedDir() / TEXT("Demos");
+    FString OldPath = ReplayDirectory / OldName;
+    FString NewPath = ReplayDirectory / NewName;
+    IFileManager::Get().Move(*NewPath, *OldPath);
+}
+
+void UReplayMenuWidget::DeleteReplay(const FString& ReplayName)
+{
+    FString ReplayDirectory = FPaths::ProjectSavedDir() / TEXT("Demos");
+    FString ReplayPath = ReplayDirectory / ReplayName;
+    IFileManager::Get().Delete(*ReplayPath);
+}
--- Source/PlaystageDemo6/ReplayMenuWidget.h
+++ Source/PlaystageDemo6/ReplayMenuWidget.h
@@ -14,4 +14,26 @@ class PLAYSTAGEDEMO6_API UReplayMenuWidget : public UUserWidget
 {
 	GENERATED_BODY()
 	
+public:
+    UFUNCTION(BlueprintCallable, Category = "Replays")
+    void StartRecording();
+
+    UFUNCTION(BlueprintCallable, Category = "Replays")
+    void StopRecording();
+
+    UFUNCTION(BlueprintCallable, Category = "Replays")
+    void ShowReplayList();
+
+    UFUNCTION(BlueprintCallable, Category = "Replays")
+    void SearchReplay(const FString& SearchText);
+
+    UFUNCTION(BlueprintCallable, Category = "Replays")
+    void RenameReplay(const FString& OldName, const FString& NewName);
+
+    UFUNCTION(BlueprintCallable, Category = "Replays")
+    void DeleteReplay(const FString& ReplayName);
+
+private:
+    UFUNCTION(BlueprintCallable, Category = "Replays")
+    TArray<FString> GetReplayList();
 };
