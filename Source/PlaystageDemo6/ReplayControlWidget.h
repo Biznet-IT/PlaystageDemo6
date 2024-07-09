84e32a22b8414c5eb97b6ee54879214338aa23bc
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2024-07-09 11:28:37 -0400

    Create ReplayControlWidget h y cpp

---
+++ Source/PlaystageDemo6/ReplayControlWidget.h
@@ -0,0 +1,17 @@
+// Biznet It 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ReplayControlWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLAYSTAGEDEMO6_API UReplayControlWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
