e2b056e90fe95f1d68fb4dc5e32412e3809734ec
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2024-07-09 11:21:35 -0400

    Create ReplayMenuWidget h y cpp

---
+++ Source/PlaystageDemo6/ReplayMenuWidget.h
@@ -0,0 +1,17 @@
+// Biznet It 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "ReplayMenuWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLAYSTAGEDEMO6_API UReplayMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
