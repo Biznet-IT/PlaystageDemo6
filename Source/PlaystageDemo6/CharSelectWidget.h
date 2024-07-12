e34eb985e84318082a62b6c8b0b969e5d1ba5df9
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2024-07-12 18:11:53 -0400

    Importacion Personajes 5

---
+++ Source/PlaystageDemo6/CharSelectWidget.h
@@ -0,0 +1,24 @@
+// Biznet It 2024
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "CharSelectWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PLAYSTAGEDEMO6_API UCharSelectWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable, Category = "Character Selection")
+    void SelectCharacter(int32 CharacterIndex);
+
+protected:
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Character Selection")
+    TArray<class UClass*> CharacterClasses;
+};
