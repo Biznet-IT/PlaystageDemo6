09b9a8442baf666f281cf08b8f21440cca8a272d
Sebastián Gómez Letelier <53100726+TheGreySam@users.noreply.github.com>
2024-07-12 10:52:43 -0400

    Funciones de Replay Control 2

--- Source/PlaystageDemo6/ReplayPlayerController.cpp
+++ Source/PlaystageDemo6/ReplayPlayerController.cpp
@@ -4,6 +4,7 @@
 #include "ReplayPlayerController.h"
 #include "Engine/World.h"
 #include "Misc/DateTime.h"
+#include "Engine/DemoNetDriver.h"
 
 FString AReplayPlayerController::GetTimestamp() const
 {
@@ -59,5 +60,88 @@ void AReplayPlayerController::DeleteReplay(const FString& ReplayName)
 	if (PlayerController)
 	{
 		PlayerController->ConsoleCommand(FString::Printf(TEXT("DeleteDemo %s"), *ReplayName), true);
+	}
+}
+
+
+
+AReplayPlayerController::AReplayPlayerController(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	bShowMouseCursor = true;
+	PrimaryActorTick.bTickEvenWhenPaused = true;
+	bShouldPerformFullTickWhenPaused = true;
+}
+
+bool AReplayPlayerController::SetCurrentReplayPausedState(bool bDoPause)
+{
+	AWorldSettings* WorldSettings = GetWorldSettings();
+	//Set MotionBlur off and Anti Aliasing to FXAA in order to bypass the pause-bug of both
+	//static const auto CVarAA = IConsoleManager::Get().FindConsoleVariable(TEXT("DefaultFeature.AntiAliasing"));
+
+	//static const auto CVarMB = IConsoleManager::Get().FindConsoleVariable(TEXT("DefaultFeature.MotionBlur"));
+
+	if (bDoPause)
+	{
+		//PreviousAASetting = CVarAA->GetInt();
+		//PreviousMBSetting = CVarMB->GetInt();
+
+		// Set MotionBlur to OFF, Anti-Aliasing to FXAA
+		//CVarAA->Set(1);
+		//CVarMB->Set(0);
+
+		WorldSettings->SetPauserPlayerState(PlayerState);
+		return true;
+	}
+	// Rest MotionBlur and AA
+	//CVarAA->Set(PreviousAASetting);
+	//CVarMB->Set(PreviousMBSetting);
+
+	WorldSettings->SetPauserPlayerState(NULL);
+	return false;
+}
+
+int32 AReplayPlayerController::GetCurrentReplayTotalTimeInSeconds() const
+{
+	if (GetWorld())
+	{
+		if (GetWorld()->GetDemoNetDriver())
+		{
+			return GetWorld()->GetDemoNetDriver()->GetDemoTotalTime();
+		}
+	}
+	return 0.f;
+}
+
+int32 AReplayPlayerController::GetCurrentReplayCurrentTimeInSeconds() const
+{
+	if (GetWorld())
+	{
+		if (GetWorld()->GetDemoNetDriver())
+		{
+			return GetWorld()->GetDemoNetDriver()->GetDemoCurrentTime();
+		}
+	}
+	return 0.f;
+}
+
+void AReplayPlayerController::SetCurrentReplayTimeToSeconds(int32 Seconds)
+{
+	if (GetWorld())
+	{
+		if (GetWorld()->GetDemoNetDriver())
+		{
+			GetWorld()->GetDemoNetDriver()->GotoTimeInSeconds(Seconds);
+		}
+	}
+}
+
+void AReplayPlayerController::SetCurrentReplayPlayRate(float PlayRate)
+{
+	if (GetWorld())
+	{
+		if (GetWorld()->GetDemoNetDriver())
+		{
+			GetWorld()->GetWorldSettings()->DemoPlayTimeDilation = PlayRate;
+		}
 	}
 }
--- Source/PlaystageDemo6/ReplayPlayerController.h
+++ Source/PlaystageDemo6/ReplayPlayerController.h
@@ -35,6 +35,28 @@ public:
 	UFUNCTION(BlueprintCallable, Category = "Replays")  
 	void DeleteReplay(const FString& ReplayName);
 
+	AReplayPlayerController(const FObjectInitializer& ObjectInitializer);
+
+	/* Set the Paused State of the Running Replay to bDoPause. Return new Pause State */
+	UFUNCTION(BlueprintCallable, Category = "CurrentReplay")
+	bool SetCurrentReplayPausedState(bool bDoPause);
+
+	/* Gets the Max Number of Seconds that were recorded in the current Replay */
+	UFUNCTION(BlueprintCallable, Category = "CurrentReplay")
+	int32 GetCurrentReplayTotalTimeInSeconds() const;
+
+	/* Gets the Second we are currently watching in the Replay */
+	UFUNCTION(BlueprintCallable, Category = "CurrentReplay")
+	int32 GetCurrentReplayCurrentTimeInSeconds() const;
+
+	/* Jumps to the specified Second in the Replay we are watching */
+	UFUNCTION(BlueprintCallable, Category = "CurrentReplay")
+	void SetCurrentReplayTimeToSeconds(int32 Seconds);
+
+	/* Changes the PlayRate of the Replay we are watching, enabling FastForward or SlowMotion */
+	UFUNCTION(BlueprintCallable, Category = "CurrentReplay")
+	void SetCurrentReplayPlayRate(float PlayRate = 1.f);
+
 private:
     FString GetTimestamp() const;
 };
